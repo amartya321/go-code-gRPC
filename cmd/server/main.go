8eb35754216e979d48e7d30c439051096710698b
Amartya Roy <amartyaroy50@gmail.com>
2025-12-21 21:04:44 +0530

    addition of create task in server side logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"log"
 	"net"
+	"strings"
+	"sync"
 	"time"
 
 	hellov1 "grpc-lab/gen/hello/v1"
+	taskv1 "grpc-lab/gen/task/v1"
 
+	"github.com/google/uuid"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type greeterServer struct {
@@ -38,14 +43,55 @@ func (s *greeterServer) SayHello(ctx context.Context, req *hellov1.HelloRequest)
 	return &hellov1.HelloReply{Message: "Hello, " + name + " 👋"}, nil
 }
 
+type TaskServiceServer struct {
+	taskv1.UnimplementedTaskServiceServer
+
+	mu        sync.Mutex
+	taskMap   map[string]*taskv1.Task
+	taskSlice []*taskv1.Task
+}
+
+func NewTaskServiceServer() *TaskServiceServer {
+	return &TaskServiceServer{
+		taskMap:   make(map[string]*taskv1.Task),
+		taskSlice: make([]*taskv1.Task, 0),
+	}
+}
+
+func (s *TaskServiceServer) CreateTask(ctx context.Context, req *taskv1.CreateTaskRequest) (res *taskv1.CreateTaskResponse, err error) {
+	title := strings.TrimSpace(req.GetTitle())
+	if title == "" {
+		return nil, status.Error(codes.InvalidArgument, "title is required")
+	}
+	id := uuid.New()
+	now := timestamppb.New(time.Now())
+	task := &taskv1.Task{
+		TaskId:      id.String(),
+		Title:       title,
+		Description: strings.TrimSpace(req.GetDescription()),
+		CreatedAt:   now,
+		UpdatedAt:   now,
+		Status:      taskv1.TaskStatus_TASK_STATUS_PENDING,
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.taskMap[task.TaskId] = task
+	s.taskSlice = append(s.taskSlice, task)
+	s.mu.Unlock()
+	return &taskv1.CreateTaskResponse{Task: task}, nil
+}
+
 func main() {
+
+	s := NewTaskServiceServer()
+
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("listen: %v", err)
 	}
 
 	grpcServer := grpc.NewServer()
-	hellov1.RegisterGreeterServer(grpcServer, &greeterServer{})
+	taskv1.RegisterTaskServiceServer(grpcServer, s)
 
 	log.Println("gRPC server listening on :50051")
 	if err := grpcServer.Serve(lis); err != nil {
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.2
 require google.golang.org/grpc v1.77.0
 
 require (
+	github.com/google/uuid v1.6.0
 	golang.org/x/net v0.46.1-0.20251013234738-63d1a5100f82 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
